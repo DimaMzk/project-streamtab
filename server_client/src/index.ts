80521e32d59de5f6dc3a197c67494fda9fa0344b
Dimitry Maizik <dima@dmaizik.ca>
2022-11-04 23:24:04 -0700

    setup electron forge

--- server_client/src/index.ts
+++ server_client/src/index.ts
@@ -1,11 +1,12 @@
 import { app, BrowserWindow } from "electron";
+import path from "path";
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
