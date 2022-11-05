a55ae5be0824ed0acc77c5640bf6357aca77e815
Dimitry Maizik <dima@dmaizik.ca>
2022-11-04 22:51:37 -0700

    Basic Electron App

---
+++ server_client/src/index.ts
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from "electron";
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile("./index.html");
+};
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
